490741dcff424411b0d54abcbc53f9800b0320b3
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-03-23 20:32:46 +0530

    feat: implement main logic

---
+++ src/main.ts
@@ -0,0 +1,88 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+setup();
+
+function setup() {
+    const dobInput = document.getElementById("dob") as HTMLInputElement;
+
+    generateCalendar(dayjs(dobInput.value));
+    // restrict future dates
+    dobInput.setAttribute("max", dayjs().format("YYYY-MM-DD"));
+    dobInput.addEventListener("input", update);
+}
+
+/**
+ * Updates the calendar based on the input event.
+ * If the years gone since the date of birth is less than 0, generates the calendar for today's date.
+ * Otherwise, generates the calendar for the date of birth.
+ *
+ * @param event - The input event that triggered the update.
+ */
+function update(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const today = dayjs();
+    const dob = dayjs(element.value);
+    const yearsGone = today.diff(dob, "year");
+
+    if (yearsGone < 0) {
+        generateCalendar(today);
+    } else {
+        generateCalendar(dob);
+    }
+}
+
+function generateCalendar(dob: dayjs.Dayjs) {
+    const today = dayjs();
+    const weeksGone = today.diff(dob, "week");
+    const yearsGone = dayjs().from(dayjs(dob), true);
+    const yearDiffElement = document.getElementById(
+        "years-diff"
+    ) as HTMLSpanElement;
+    yearDiffElement.innerText = yearsGone;
+
+    const fragment = document.createDocumentFragment();
+
+    let i = 0;
+
+    for (let j = 0; j < 4; ++j) {
+        const innerListFragment = document.createDocumentFragment();
+
+        for (let k = 0; k < 10; ++k) {
+            const singleLineFragment = document.createDocumentFragment();
+
+            for (let l = 0; l < 100; ++l) {
+                const box = document.createElement("div");
+                box.classList.add("box");
+                if (i < weeksGone) {
+                    box.classList.add("fill");
+                }
+                singleLineFragment.insertBefore(
+                    box,
+                    singleLineFragment.firstChild
+                );
+
+                ++i;
+            }
+
+            const singleLine = document.createElement("div");
+            singleLine.classList.add("singleline");
+            singleLine.appendChild(singleLineFragment);
+            innerListFragment.insertBefore(
+                singleLine,
+                innerListFragment.firstChild
+            );
+        }
+
+        const innerList = document.createElement("div");
+        innerList.classList.add("innerlist");
+        innerList.appendChild(innerListFragment);
+        fragment.insertBefore(innerList, fragment.firstChild);
+    }
+
+    const container = document.getElementById("container") as HTMLDivElement;
+    container.innerHTML = "";
+
+    container.appendChild(fragment);
+}
